6c600f8a4d560a9dd58b661da6520c5374682bc2
AnhBinh23 <nbinh2612@gmail.com>
2026-08-19 15:22:11 +0700

    fix: remove unused imports

--- src/pages/admin/classes/FlexibleManage.jsx
+++ src/pages/admin/classes/FlexibleManage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import MainLayout from '../../../components/layout/MainLayout';
-import Button from '../../../components/ui/Button';
 import api from '../../../services/api';
 import { toast } from 'react-toastify';
 
